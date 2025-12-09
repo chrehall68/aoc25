9e5e74320d2ebba4f53ef53230f62198f0570576
chrehall68 <chrehall68@gmail.com>
2025-12-08 18:04:45 -0800

    day 4, part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,7 @@
-use std::io::stdin;
+use std::{
+    collections::{HashMap, HashSet},
+    io::stdin,
+};
 
 fn get_inp() -> Vec<String> {
     let mut inp = String::new();
@@ -12,9 +15,9 @@ fn get_inp() -> Vec<String> {
     res
 }
 
-fn part1(rows: &Vec<String>) -> i64 {
-    let mut total = 0;
+fn indegrees(rows: &Vec<String>) -> HashMap<(i32, i32), i32> {
     let cols = rows[0].len();
+    let mut res = HashMap::new();
     for r in 0..rows.len() {
         for c in 0..cols {
             // check all neighbors
@@ -37,16 +40,84 @@ fn part1(rows: &Vec<String>) -> i64 {
                         }
                     }
                 }
-                if neighbors < 4 {
-                    total += 1;
-                }
+                res.insert((r as i32, c as i32), neighbors);
             }
         }
     }
 
-    total
+    res
 }
+
+fn reachable(rows: &Vec<String>, mut indegrees: HashMap<(i32, i32), i32>) -> usize {
+    // basically topsort
+    // but instead of allowing once indegree == 0,
+    // we now allow once indegree < 4
+    let mut to_process: Vec<(i32, i32)> = indegrees
+        .iter()
+        .filter_map(|(k, v)| if *v < 4 { Some(*k) } else { None })
+        .collect();
+    let mut used: HashSet<(i32, i32), _> = to_process.iter().cloned().collect::<HashSet<_>>();
+    let mut idx = 0;
+    while idx < to_process.len() {
+        let (r, c) = to_process[idx];
+        // decrement all neighbors
+        for dr in -1..=1 {
+            for dc in -1..=1 {
+                let rp = r + dr;
+                let cp = c + dc;
+                let key = (rp, cp);
+                if indegrees.contains_key(&key) {
+                    let deg = indegrees.get(&key).unwrap() - 1;
+                    indegrees.insert(key, deg);
+                    if deg == 3 {
+                        used.insert(key);
+                        to_process.push(key);
+                    }
+                }
+            }
+        }
+        idx += 1;
+    }
+
+    to_process.len()
+}
+
 pub fn driver() {
     let rows = get_inp();
-    println!("{}", part1(&rows));
+    let starter = indegrees(&rows);
+    println!("{}", starter.values().filter(|v| **v < 4).count());
+    println!("{}", reachable(&rows, starter));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    const ROWS: [&str; 10] = [
+        "..@@.@@@@.",
+        "@@@.@.@.@@",
+        "@@@@@.@.@@",
+        "@.@@@@..@.",
+        "@@.@@@@.@@",
+        ".@@@@@@@.@",
+        ".@.@.@.@@@",
+        "@.@@@.@@@@",
+        ".@@@@@@@@.",
+        "@.@.@@@.@.",
+    ];
+
+    #[test]
+    fn test_part1_example() {
+        let v = Vec::from(ROWS).into_iter().map(|s| s.to_owned()).collect();
+        let starter = indegrees(&v);
+        let res = starter.values().filter(|v| **v < 4).count();
+        assert_eq!(res, 13);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let v = Vec::from(ROWS).into_iter().map(|s| s.to_owned()).collect();
+        let starter = indegrees(&v);
+        let res = reachable(&v, starter);
+        assert_eq!(res, 43);
+    }
 }
